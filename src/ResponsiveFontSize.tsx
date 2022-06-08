4c2e63f34c1dcbfc96e2a10471636a774fa4d688
Paul Engelmeier <allwqre@pm.me>
2022-06-08 16:44:47 +0200

    fix: font increasing size of container

--- src/ResponsiveFontSize.tsx
+++ src/ResponsiveFontSize.tsx
@@ -1,13 +1,23 @@
-import React from "react";
-import { FC } from "react";
-import useResponsiveFontSize, { IuseResponsiveFontSizeProps } from "./useResponsiveFontSize";
+import React, { CSSProperties, ReactNode, FC } from "react";
+import useResponsiveFontSize, { OptionsObject } from "./useResponsiveFontSize";
 
+interface UseResponsiveFontSizeProps {
+  children: ReactNode;
+  ratio: number;
+  className?: string;
+  style?: CSSProperties;
+  optionsObject?: OptionsObject;
+}
 
-const ResponsiveFontSize: FC<IuseResponsiveFontSizeProps> = ({ children, ratio, optionsObject }) => {
+const ResponsiveFontSize: FC<UseResponsiveFontSizeProps> = ({ children, ratio, className, style, optionsObject }) => {
   const ref = useResponsiveFontSize(ratio, optionsObject);
 
   return (
-    <div ref={ref} style={{ width: "100%", height: "100%" }}>
+    <div
+      ref={ref}
+      className={className ?? ""}
+      style={{ ...style, width: "100%", height: "100%", boxSizing: "border-box" }}
+    >
       {children}
     </div>
   );
